ef374c2e503b0fd119743ef2e8bf1499e6bf0691
Artem <artemtruskiyweb@gmail.com>
2022-08-10 14:59:16 +0300

    first commit

---
+++ gulp/config/path.js
@@ -0,0 +1,40 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve())
+
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+
+export const path = {
+   build: {
+      js: `${buildFolder}/js/`,
+      pug: `${buildFolder}/`,
+      html: `${buildFolder}/`,
+      files: `${buildFolder}/files/`,
+      images: `${buildFolder}/img/`,
+      css: `${buildFolder}/css/`,
+      // fonts: `${buildFolder}/fonts/`,
+   },
+   src: {
+      js: `${srcFolder}/js/app.js`,
+      pug: `${srcFolder}/*.pug`,
+      html: `${srcFolder}/*.html`,
+      files: `${srcFolder}/files/**/*.*`,
+      scss: `${srcFolder}/scss/style.scss`,
+      images: `${srcFolder}/img/**/*.png`,
+      svg: `${srcFolder}/img/**/*.svg`,
+      svgicons: `${srcFolder}/svgicons/*.svg`,
+   },
+   watch: {
+      js: `${srcFolder}/js/**/*.js`,
+      pug: `${srcFolder}/**/*.pug`,
+      html: `${srcFolder}/**/*.html`,
+      files: `${srcFolder}/files/**/*.*`,
+      scss: `${srcFolder}/scss/**/*.scss`,
+      images: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp, svg}`,
+   },
+   clean: buildFolder,
+   buildFolder: buildFolder,
+   srcFolder: srcFolder,
+   rootFolder: rootFolder,
+   ftp: ``
+}
